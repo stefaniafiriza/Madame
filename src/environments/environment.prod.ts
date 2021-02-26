a064107b3d00c29fd5800542100d3088cb40e8e9
Firiza Stefania <stefaniafiriza@gmail.com>
2021-02-26 11:35:08 +0200

    Connection with firebase.Deploy site

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBKOn4FyBMI0l8Ifj0leYMvCtu7OiMGRQw",
+    authDomain: "madame-46a72.firebaseapp.com",
+    projectId: "madame-46a72",
+    storageBucket: "madame-46a72.appspot.com",
+    messagingSenderId: "415165544436",
+    appId: "1:415165544436:web:a9fe676308d2c4fb892ba1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBKOn4FyBMI0l8Ifj0leYMvCtu7OiMGRQw",
+    authDomain: "madame-46a72.firebaseapp.com",
+    projectId: "madame-46a72",
+    storageBucket: "madame-46a72.appspot.com",
+    messagingSenderId: "415165544436",
+    appId: "1:415165544436:web:a9fe676308d2c4fb892ba1"
+  }
 };
 
 /*
