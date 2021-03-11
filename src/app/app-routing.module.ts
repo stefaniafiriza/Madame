a56d607a9e9400b9804625ae7e2c97bbca7cdb0c
Firiza Stefania <stefaniafiriza@gmail.com>
2021-03-11 18:24:01 +0200

    Register front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from '../app/component/home/home.component';
+import { RegisterComponent } from '../app/component/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
